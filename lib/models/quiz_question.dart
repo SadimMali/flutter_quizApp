fe96844b2c47a86b9c2a50f4f1d471b902431e4a
Sadim Mali <sadimmalakar77@gmail.com>
2025-03-04 22:48:38 +0545

    feat: Add method to shuffle answers in QuizQuestion model

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -2,4 +2,10 @@ class QuizQuestion {
   const QuizQuestion(this.text, this.answers);
   final String text;
   final List<String> answers;
+
+  List<String> getShuffledAnswers() {
+    final shulffledAnswers = List.of(answers);
+    shulffledAnswers.shuffle();
+    return shulffledAnswers;
+  }
 }
