66efd3854cc1ded651bc8c5957ab593807a1d758
Sadim Mali <sadimmalakar77@gmail.com>
2025-03-08 09:49:05 +0545

    feat: Enhance QuestionSummary layout with improved styling and answer feedback

--- lib/question_summary.dart
+++ lib/question_summary.dart
@@ -11,39 +11,70 @@ class QuestionSummary extends StatelessWidget {
       height: 300,
       child: SingleChildScrollView(
         child: Column(
+            spacing: 15,
             children: summaryData.map(
-          (data) {
-            return Row(spacing: 30, children: [
-              Text(
-                ((data['question_index'] as int) + 1).toString(),
-              ),
-              Expanded(
-                child: Column(
-                  children: [
-                    Text(
-                      data['question'] as String,
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16),
-                    ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    Text(
-                      data['correct_answer'] as String,
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16),
-                    ),
-                    Text(data['user_answer'] as String),
-                  ],
-                ),
-              ),
-            ]);
-          },
-        ).toList()),
+              (data) {
+                return Row(
+                    spacing: 30,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: 40,
+                        width: 40,
+                        alignment: Alignment.center,
+                        padding: const EdgeInsets.all(
+                            10), // Padding inside the number
+                        decoration: BoxDecoration(
+                          color: data['correct_answer'] == data['user_answer']
+                              ? Colors.cyan
+                              : Colors.purpleAccent, // Green background
+                          borderRadius:
+                              BorderRadius.circular(20), // Rounded corners
+                        ),
+                        child: Text(
+                          ((data['question_index'] as int) + 1).toString(),
+                          style: const TextStyle(
+                            // color: Colors., // White text color
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              data['question'] as String,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 17,
+                              ),
+                              textAlign: TextAlign.left,
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              data['correct_answer'] as String,
+                              style: TextStyle(
+                                  color:
+                                      const Color.fromARGB(255, 170, 159, 255),
+                                  fontSize: 14),
+                            ),
+                            Text(
+                              data['user_answer'] as String,
+                              style: TextStyle(
+                                  color:
+                                      const Color.fromARGB(255, 198, 255, 198),
+                                  fontSize: 14),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ]);
+              },
+            ).toList()),
       ),
     );
   }
