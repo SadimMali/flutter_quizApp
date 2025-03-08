989ae03b35854d190083418b9cb2eb6572d0d18f
Sadim Mali <sadimmalakar77@gmail.com>
2025-03-08 09:49:19 +0545

    feat: Add restart functionality to ResultsScreen with improved UI

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -3,9 +3,12 @@ import 'package:adv_basics/question_summary.dart';
 import 'package:flutter/material.dart';
 
 class ResultsScreen extends StatelessWidget {
-  ResultsScreen({super.key, required this.chosenAnswers});
+  const ResultsScreen(this.restartQuiz,
+      {super.key, required this.chosenAnswers});
 
   final List<String> chosenAnswers;
+
+  final void Function() restartQuiz;
 
   List<Map<String, Object>> getSummary() {
     List<Map<String, Object>> summary = [];
@@ -27,9 +30,11 @@ class ResultsScreen extends StatelessWidget {
   @override
   Widget build(context) {
     final numTotalQuestions = questions.length;
-    final numCorrectAnswer = getSummary().where((data) {
-      return data['correct_answer'] == data['user_answer'];
-    }).length;
+    final numCorrectAnswer = getSummary()
+        .where(
+          (data) => data['correct_answer'] == data['user_answer'],
+        )
+        .length;
 
     return SizedBox(
       width: double.infinity,
@@ -44,7 +49,7 @@ class ResultsScreen extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                   fontSize: 20,
                   color: const Color.fromARGB(255, 241, 160, 246)),
-                  textAlign: TextAlign.center,
+              textAlign: TextAlign.center,
             ),
             SizedBox(
               height: 30,
@@ -54,10 +59,20 @@ class ResultsScreen extends StatelessWidget {
             SizedBox(
               height: 30,
             ),
-            TextButton(
-              onPressed: () {},
-              child: Text('Restart_quiz'),
-            )
+            TextButton.icon(
+              style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  textStyle: TextStyle(
+                    fontSize: 18,
+                  )),
+              onPressed: restartQuiz,
+              icon: const Icon(
+                Icons.refresh,
+                color: Colors.white,
+                size: 25,
+              ),
+              label: Text('Restart Quiz!'),
+            ),
           ],
         ),
       ),
